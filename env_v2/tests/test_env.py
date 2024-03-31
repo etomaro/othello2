1e1c6d0f46d09afb4fc739ffe6fdc4546b64bbc4
root <root@UBUNTU>
2024-03-31 22:23:55 +0900

    PYTHONPATHの設定方法を修正

--- env_v2/tests/test_env.py
+++ env_v2/tests/test_env.py
@@ -1,2 +1,11 @@
 import pytest 
 from env_v2.env import Env2
+
+
+class TestEnv2:
+    
+    def setup_method(self):
+        print("setup")
+        
+    def test_a(self):
+        self.assertEqual(1, 1)
