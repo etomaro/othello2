d66403676c1fbe33c9f6676e52b9763e31daa036
root <root@UBUNTU>
2024-04-02 14:14:21 +0900

    テストメモを追加

--- env_v2/tests/test_env.py
+++ env_v2/tests/test_env.py
@@ -15,7 +15,7 @@ class TestEnv2(unittest.TestCase):
     def setUp(self):
         # print("setup")
         
-        self._env = Env2(is_out_game_info=True, is_out_board=True)
+        self._env = Env2(is_out_game_info=False, is_out_board=False)
     
     def tearDown(self) -> None:
         # print("tearDown")
@@ -27,14 +27,14 @@ class TestEnv2(unittest.TestCase):
         pass
         
     def test_random_play(self):
-        """
+        """ 
         randomでプレイさせてエラーにならないこと(ゲームができること)
         """
         random_player_black = RandomPlayer(PlayerId.BLACK_PLAYER_ID.value)
         random_player_white = RandomPlayer(PlayerId.WHITE_PLAYER_ID.value)
         
         # 100回プレイしてエラーが発生しないこと
-        play_num = 1
+        play_num = 1000
         for play_count in range(1, play_num+1):
             # ゲームの初期化
             game_info = self._env.get_game_init()
@@ -52,6 +52,16 @@ class TestEnv2(unittest.TestCase):
                 if game_info.game_state.value[0] != GameState.IN_GAME.value[0]:
                     print(f"test play count: {play_count} is ok")
                     break
+    
+    # TODO: テストを追加する
+    """
+    通常のストーリーを3つくらい
+    どっちも打つ手がなくなって途中でゲームが終了するパターン
+    途中でどちらかの打つ手がなくなって連続でアクションするパターン
+    途中ですべてどちらかの駒で埋まるパターン
+    最後までいって引き分けで終了するパターン
+    途中で終わるが引き分けのパターン
+    """
     
             
             
