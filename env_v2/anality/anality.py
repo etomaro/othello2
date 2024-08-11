5f70a18cf96ec9d02fe8d45177c725b507f1ae35
root <root@UBUNTU>
2024-08-12 04:41:02 +0900

    リファクタリング

--- env_v2/anality/anality.py
+++ env_v2/anality/anality.py
@@ -2,7 +2,8 @@
 世代ごとの状態数を算出する
 """
 from env_v2.anality.not_batch import not_batch
-from env_v2.anality.batch3 import run
+from env_v2.anality.batch4 import run as run_v4
+from env_v2.anality.batch5 import run as run_v5
 from env_v2.anality.batch2 import batch as batch_for_anality
 from env_v2.anality.batch import batch
 
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     # not_batch(8)
     
     # batch
-    run(3)
+    run_v5(3)
     # for i in range(0, 10):
     #     print("-----------start-----------")
     #     run(i)
--- env_v2/anality/setup.py
+++ env_v2/anality/setup.py
@@ -4,3 +4,6 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize("ray_calc_next_states_cython.pyx")
 )
+setup(
+    ext_modules=cythonize("env_for_anality_cython.pyx")
+)
