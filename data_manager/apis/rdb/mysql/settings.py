a7a57cd550a4860d958edeb09f6afc4a8bdceaf7
root <root@UBUNTU>
2024-10-21 23:31:02 +0900

    mysql containerのport設定をfix

--- data_manager/apis/rdb/mysql/settings.py
+++ data_manager/apis/rdb/mysql/settings.py
@@ -2,40 +2,40 @@
 HOST_DEV = 'mysql_dev'
 USER_DEV = 'root'
 PASS_DEV = 'pass'
-PORT_DEV = 3306
+PORT_DEV = 3307
 
 # テスト用MySQLサーバーの接続情報
 HOST_TEST = 'mysql_test'
 USER_TEST = 'root'
 PASS_TEST = 'pass'
-PORT_TEST = 3307
+PORT_TEST = 3308
 
 # 1万データ用意したMYSQL
 HOST_10000 = 'mysql_10000'
 USER_10000 = 'root'
 PASS_10000 = 'pass'
-PORT_10000 = 3308
+PORT_10000 = 3309
 
 # 10万データ用意したMYSQL
 HOST_100000 = 'mysql_100000'
 USER_100000 = 'root'
 PASS_100000 = 'pass'
-PORT_100000 = 3309
+PORT_100000 = 3310
 
 # 100万データ用意したMYSQL
 HOST_1million = 'mysql_1million'
 USER_1million = 'root'
 PASS_1million = 'pass'
-PORT_1million = 3310
+PORT_1million = 3311
 
 # 1000万データ用意したMYSQL
 HOST_10million = 'mysql_10million'
 USER_10million = 'root'
 PASS_10million = 'pass'
-PORT_10million = 3311
+PORT_10million = 3312
 
 # 1億データ用意したMYSQL
 HOST_100million = 'mysql_100million'
 USER_100million = 'root'
 PASS_100million = 'pass'
-PORT_100million = 3312
+PORT_100million = 3313
