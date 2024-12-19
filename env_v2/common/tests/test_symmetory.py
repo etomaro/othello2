5d596aab2b9bd0c109dd7a50b86461d51a04550d
etomaro <python.helloworld111@gmail.com>
2024-12-19 15:34:20 +0900

    unittestが通るようにinitファイルを作成

---
+++ env_v2/common/tests/test_symmetory.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestSymmetory(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        pass
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self) -> None:
+        pass
+    
+    @classmethod
+    def tearDownClass(cls):
+        pass
+        
+    def test_a(self):
+        self.assertEqual(1,1)
