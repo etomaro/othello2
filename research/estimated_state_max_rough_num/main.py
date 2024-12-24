9cd1871116079e7214676110f882ba6ba12af67a
etomaro <python.helloworld111@gmail.com>
2024-12-24 19:59:18 +0900

    基本構造を作成

---
+++ research/estimated_state_max_rough_num/main.py
@@ -0,0 +1,74 @@
+import time 
+import os
+import csv
+from datetime import datetime 
+from zoneinfo import ZoneInfo
+
+
+def _sec_to_str(calc_time: int) -> str:
+    """
+    秒を{hour}h{minute}m{seconds}sの形式に変換する
+    """
+    minute_slot = 60
+    hour_slot = 60*60
+
+    calc_time_str = ""
+    if (calc_time // hour_slot) >= 1:
+        # 1時間以上の場合
+        hour = calc_time // hour_slot
+        calc_time -= hour * hour_slot
+        calc_time_str += f"{hour}h"
+    if (calc_time // minute_slot) >= 1:
+        # 1分以上の場合
+        minute = calc_time // minute_slot 
+        calc_time -= minute * minute_slot 
+        calc_time_str += f"{minute}m"
+    # 秒の追加
+    calc_time_str += f"{calc_time}s"
+
+    return calc_time_str
+
+def calc(generation: int) -> int:
+    """
+    世代ごとの雑な推定最大状態数を求める
+
+    1. 中心を除いた石のおけるパターン数を算出(=中心を除いた60マスの内(黒と白のおける石の数)-4(中心のマス)のパターン数)
+    2. 石の数より黒と白のおける石のパターンを求める
+    3. 2で求めたパターンごとに黒と白のマスのとり方を求める
+    4. 1で求めた数 * 3で求めた数より世代の雑な推定最大状態数を求める
+
+    args:
+      generation: 世代
+    """
+    stone_num = generation + 4  # 石の数
+    stone_num_without_center = generation  # 中心マス以外の石の数
+
+    # 1. 中心を除いた石のおけるパターン数を算出(=中心を除いた60マスの内(黒と白のおける石の数)-4(中心のマス)のパターン数)
+    
+if __name__ == "__main__":
+    header = ["世代", "推定状態数", "計測時間"]
+    datas = {}  # {generation: {estimated_num: N, calc_time: M}}
+    for generation in range(1, 60+1):
+        print(f"世代: {generation} calc start.")
+        
+        start_time = time.time()
+
+        # 計算
+        estimated_num = calc(generation)
+
+        calc_time_sec = int(time.time() - start_time)
+        calc_time_str = _sec_to_str(calc_time_sec)
+
+        datas[generation] = {"estimated_num": estimated_num, "calc_time": calc_time_str}
+        
+        print(f"世代: {generation} calc end.")
+    
+    # CSV出力
+    base_folder = os.path.dirname(__file__)
+    file_path = base_folder + "/" + f"estimated_state_max_rough_num.csv"
+    with open(file_path, "w") as f:
+        writer = csv.writer(f)
+        rows = [header]
+        for generation, data in datas.items():
+            rows.append([str(generation), data["estimated_num"], data["calc_time"]])
+        writer.writerows(rows)
