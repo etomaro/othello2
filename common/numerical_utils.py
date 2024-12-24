e8de06b753b6a318c1e22bea568d988147637b85
etomaro <python.helloworld111@gmail.com>
2024-12-24 22:43:09 +0900

    雑な推定最大状態数の処理を作成

---
+++ common/numerical_utils.py
@@ -0,0 +1,9 @@
+
+
+
+def get_with_jp_unit(value: int) -> str:
+    """
+    万や億などの単位をつけて返す
+    """
+    # 単位(key=10の乗数, value=万や億などの単位)
+    pass
--- research/estimated_state_max_rough_num/main.py
+++ research/estimated_state_max_rough_num/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from common.dt_utils import sec_to_str
+from common.n_C_r import json_utils
 
 
 
@@ -24,6 +25,35 @@ def calc(generation: int) -> int:
     stone_num_without_center = generation  # 中心マス以外の石の数
 
     # 1. 中心を除いた石のおけるパターン数を算出(=中心を除いた60マスの内(黒と白のおける石の数)-4(中心のマス)のパターン数)
+    stone_pos_patter = json_utils.read_json_n_c_r(60)[str(stone_num_without_center)]
+
+    # 2. 石の数より黒と白のおける石のパターンを求める
+    stone_num_pattern = []
+    for black_stone_num in range(stone_num+1):
+        # 合計石数の半数を超えた場合ループ終了
+        if black_stone_num > (stone_num/2):
+            break
+    
+        white_stone_num = stone_num - black_stone_num
+        stone_num_pattern.append((black_stone_num, white_stone_num))
+    
+    # 3. 2で求めたパターンごとに黒と白のマスのとり方を求める
+    black_white_pattern = {}
+    for black_num, white_num in stone_num_pattern:
+        min_num = min(black_num, white_num)
+        # (stone_num)C(min_num)
+        black_white_pattern[(black_num, white_num)] = json_utils.read_json_n_c_r(stone_num)[str(min_num)]
+    
+    # 4. 1で求めた数 * 3で求めた数より世代の雑な推定最大状態数を求める
+    estimated_num = 0
+    for black_white_num, value in black_white_pattern.items():
+        black_num, white_num = black_white_num 
+        if black_num == white_num:
+            estimated_num += (stone_pos_patter*value) 
+        else:
+            estimated_num += (stone_pos_patter*value)*2
+
+    return estimated_num
 
 if __name__ == "__main__":
     header = ["世代", "推定状態数", "計測時間"]
