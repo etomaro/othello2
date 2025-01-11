4c5757c97ab43e9eadc46ffdd708508352a8129f
etomaro <python.helloworld111@gmail.com>
2025-01-12 06:03:17 +0900

    共通関数の不具合修正

--- common/numerical_utils.py
+++ common/numerical_utils.py
@@ -7,6 +7,7 @@ def get_with_jp_unit(value: int) -> str:
     """
     # 単位(key=10の乗数, value=万や億などの単位)
     unit_map = {
+        0: "",
         4: "万",
         8: "億",
         12: "兆",
@@ -20,13 +21,25 @@ def get_with_jp_unit(value: int) -> str:
         raise Exception("value is over 10^32.")
     
     value_with_jp_unit_list = []
-    value_with_jp_unit_list.append(str(value)[-4:])  # 最初
-    for key, unit in unit_map.items():
-        if key > power_of_ten:
-            value_with_jp_unit_list.append(str(value)[-key-4: -key])
-            break
+    for i in range(0, len(str(value)), 4):
+        if i == 0:
+            sdx = -4
+            _value = str(value)[sdx:]
+            value_with_jp_unit_list.append(str(int(_value)) + unit_map[i])
         else:
-            value_with_jp_unit_list.append(str(value)[-key-4: -key] + unit)
+            sdx = -1 * (i+4) 
+            edx = -1 * i 
+            _value = str(value)[sdx:edx]
+            value_with_jp_unit_list.append(str(int(_value)) + unit_map[i])
+
+    # num = str(value)[-4:]  # 最初
+    # value_with_jp_unit_list.append(str(value)[-4:])  # 最初
+    # for key, unit in unit_map.items():
+    #     if key > power_of_ten:
+    #         value_with_jp_unit_list.append(str(value)[-key-4: -key])
+    #         break
+    #     else:
+    #         value_with_jp_unit_list.append(str(value)[-key-4: -key] + unit)
     
     value_with_jp_unit_list.reverse()
     return "".join(value_with_jp_unit_list)
