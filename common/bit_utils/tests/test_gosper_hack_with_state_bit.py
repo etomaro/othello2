9fe1279d67bbec098d496f212f600918f562708e
etomaro <python.helloworld111@gmail.com>
2025-01-21 02:29:17 +0900

    test comment add

--- common/bit_utils/tests/test_gosper_hack_with_state_bit.py
+++ common/bit_utils/tests/test_gosper_hack_with_state_bit.py
@@ -20,6 +20,11 @@ class TestGosperHackWithStateBit(unittest.TestCase):
     def tearDownClass(cls):
         # print("tearDownClass")
         pass
-        
+    
+    """
+    1. 件数テスト(※時間がかかるため60C(1-5)と60C(56-60)
+    2. 既存の60Cgenerationを求めるものと計算結果が同じか
+    3. 既存の60Cgenerationを求めるものとの速度調査
+    """
     def test_tmp(self):
         self.assertTrue(True)
