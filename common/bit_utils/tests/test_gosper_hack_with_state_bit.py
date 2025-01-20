3e51e6c9241f42847072e2d54dce4846a3e0e67d
etomaro <python.helloworld111@gmail.com>
2025-01-21 02:16:24 +0900

    test base make

---
+++ common/bit_utils/tests/test_gosper_hack_with_state_bit.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class TestGosperHackWithStateBit(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        # print("setUpClass")
+        pass
+    
+    def setUp(self):
+        # print("setup")
+        pass
+    
+    def tearDown(self) -> None:
+        # print("tearDown")
+        pass
+    
+    @classmethod
+    def tearDownClass(cls):
+        # print("tearDownClass")
+        pass
+        
+    def test_tmp(self):
+        self.assertTrue(True)
